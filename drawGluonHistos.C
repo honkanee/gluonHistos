7fbe10c17f753d1a7b743a8fe69557b082917a0d
Eetu Honkanen <eetu.honkanen@tuni.fi>
2021-06-01 13:51:31 +0300

    small refactoring

--- drawGluonHistos.C
+++ drawGluonHistos.C
@@ -8,25 +8,25 @@
 void drawGluonHistos() {
     TFile* f = TFile::Open("outputGluonHistos.root");
 
-    TProfile* jetPt_genJetPt;
-    TProfile* jetPt_genJetPt_w_PF;
+    TProfile* jetPt_genJetPt_ratio;
+    TProfile* jetPt_genJetPt_ratio_w_PF;
 
-    f->GetObject("jetPt_genJetPt", jetPt_genJetPt);
-    f->GetObject("jetPt_genJetPt, nGenJetPF", jetPt_genJetPt_w_PF);
+    f->GetObject("jetPt_genJetPt", jetPt_genJetPt_ratio);
+    f->GetObject("jetPt_genJetPt, nGenJetPF", jetPt_genJetPt_ratio_w_PF);
 
-    jetPt_genJetPt->SetMarkerColor(kBlack);
-    jetPt_genJetPt->SetLineColor(kBlack);
-    jetPt_genJetPt->SetLineWidth(2);
+    jetPt_genJetPt_ratio->SetMarkerColor(kBlack);
+    jetPt_genJetPt_ratio->SetLineColor(kBlack);
+    jetPt_genJetPt_ratio->SetLineWidth(2);
 
-    jetPt_genJetPt_w_PF->SetMarkerColor(kRed);
-    jetPt_genJetPt_w_PF->SetLineColor(kRed);
-    jetPt_genJetPt_w_PF->SetLineWidth(2);
+    jetPt_genJetPt_ratio_w_PF->SetMarkerColor(kRed);
+    jetPt_genJetPt_ratio_w_PF->SetLineColor(kRed);
+    jetPt_genJetPt_ratio_w_PF->SetLineWidth(2);
 
     TCanvas* c = new TCanvas("c","",900,700);
 
-    jetPt_genJetPt->SetAxisRange(0.97, 1.03, "Y");
-    jetPt_genJetPt->GetXaxis()->SetTitle("jetPt");
+    jetPt_genJetPt_ratio->SetAxisRange(0.97, 1.03, "Y");
+    jetPt_genJetPt_ratio->GetXaxis()->SetTitle("jetPt");
 
-    jetPt_genJetPt->Draw();
-    jetPt_genJetPt_w_PF->Draw("same");
+    jetPt_genJetPt_ratio->Draw();
+    jetPt_genJetPt_ratio_w_PF->Draw("same");
 }
